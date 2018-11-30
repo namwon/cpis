aeed3b7a7800ce86245cf1bd271f072820c73b5d
Sutthanit Chaikhunpol <namwon74@hotmail.com>
2018-11-30 15:59:59 +0700

    lib\nusoap

---
+++ chklogin.php
@@ -0,0 +1,39 @@
+<?php
+header('Content-type: text/html; charset=UTF-8');
+include('include/inc_conn.php');
+include('include/function.php');
+
+$user_login = $_POST['username'];
+$pass_login = $_POST['password'];
+
+include("assts/lib/nusoap.php");
+$client = new nusoap_client("http://arya1/itservice/WebServiceServer.php?wsdl",true);
+$params = array(
+           'uSername' => $user_login,
+           'pAssword' => $pass_login
+);
+$data = $client->call("cLogin",$params);
+
+ if ($data[0] == 'ok') {
+
+  $_SESSION['loginok'] = $data[0];
+  $_SESSION['user_id'] = $data[1];
+  $_SESSION['user_code'] = $data[2];
+  $_SESSION['user_login'] = $data[3];
+  $_SESSION['off_name'] = $data[4];
+  $_SESSION['dep_code'] = $data[5];
+  $_SESSION['dep_name'] = $data[6];
+  $_SESSION['post_name'] = $data[7];
+  $_SESSION['off_id'] = $data[8];
+
+ 	echo "success,success";
+} else {
+	echo "error,Invalid Username or Password!";
+}
+
+ // foreach($data as $val)
+ // {
+ //   echo $val."<br>";
+ // }
+
+?>
---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="th" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ระบบสารสนเทศศาลอาญา</title>
+    <link rel="shortcut icon" href="assets/img/logo32x32.png">
+
+    <link rel="stylesheet" type="text/css" href="assets/plugins/bootstrap-4.1.3/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+  </head>
+  <body>
+    <div class="login-page">
+      <div class="form">
+        <center style="margin-left: -15px;">
+          <img src="assets/img/logo.png" alt="">
+        </center>
+        <hr>
+        <form class="login-form" action="chklogin.php">
+          <input type="text" name="username" placeholder="username"/>
+          <input type="password" name="password" placeholder="password"/>
+          <!-- <a href="index.php?inc=history" class="button">Login</a> -->
+          <button type="button">login</button>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
