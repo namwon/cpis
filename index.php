e06f1a2f0c41dd8a6eab21cd31de907b6bd2dbc1
Sutthanit Chaikhunpol <namwon74@hotmail.com>
2018-12-14 03:27:02 +0700

    logo name project

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if (!empty($_GET['inc'])) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ระบบสารสนเทศศาลอาญา</title>
+    <title>ระบบสารสนเทศทะเบียนประวัติศาลอาญา</title>
     <link rel="shortcut icon" href="assets/img/logo32x32.png">
 
     <!-- Bootstrap -->
--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ระบบสารสนเทศศาลอาญา</title>
+    <title>ระบบสารสนเทศทะเบียนประวัติศาลอาญา</title>
     <link rel="shortcut icon" href="assets/img/logo32x32.png">
 
     <link rel="stylesheet" href="assets/plugins/bootstrap-4.1.3/dist/css/bootstrap.css">
